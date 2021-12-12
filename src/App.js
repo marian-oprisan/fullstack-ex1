df5e67be20d7381fb69a81eff9619dc85a2bd91f
Marian Oprisan <marian.razvan.oprisan@gmail.com>
2021-12-12 21:29:16 +0200

    Finished exercises

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
+  
+  function decrementCounter() {
+    if(counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
+  const fruits = ["Avocado", "Apricot", "Strawberry", "Banana", "Peach"];
+
+  const [inputText, setInputText] = useState(' ');
+  function onInputChange(e) {
+    setInputText(e.target.value);
+    document.title = e.target.value;
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Exercises</h1>
+
+      <h3>Counter</h3>
+      <div className="counter">
+        <button className="decrement-btn" onClick={decrementCounter}>-</button>
+        <div className="count">{counter}</div>
+        <button className="increment-btn" onClick={incrementCounter}>+</button>
+      </div>
+
+      <h3>List</h3>
+      <div className="list-field">
+        <ul className="list">
+          {fruits.map((fruit, index) => (
+            <li className="list-item" key={index}>{fruit}</li>
+          ))}
+        </ul>
+      </div>
+
+      <h3>Input Element</h3>
+      <div className="inputDisplay">
+        <h3>You entered: {inputText}</h3>
+        <input 
+          type="text"  
+          value={inputText}
+          onChange={onInputChange}
+          />
+      </div>
+
     </div>
   );
 }
